555b86e50018ffbb0999ddb2999e7f534230792e
Altan Duman <halicedu@gmail.com>
2026-05-12 23:51:49 +0300

    Initial Commit

---
+++ Flare/FlareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlareApp.swift
+//  Flare
+//
+//  Created by Altan Duman on 12.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
